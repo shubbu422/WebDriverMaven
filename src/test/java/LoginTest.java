12128985f90f044184b3edfbb1848abd4a1040f2
unknown <shubham.vernekar14@gmail.com>
2020-01-11 00:25:36 +0530

    Initial Commit

---
+++ src/test/java/LoginTest.java
@@ -0,0 +1,36 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LoginTest {
+	
+	public static WebDriver driver;
+	
+	@BeforeSuite
+	public void setUp() {
+		
+		//System.setProperty("webdriver.chrome.driver", "C:\\Users\\Shubham Vernekar\\Documents\\jar files\\geckodriver.exe");
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		
+	}
+	
+	@Test
+	public void doLogin() {
+		
+		driver.get("http://gmail.com");
+		driver.findElement(By.id("identifierId")).sendKeys("appcompatian66@gmail.com");
+		driver.findElement(By.xpath("//*[@id=\"identifierNext\"]/span/span")).click();
+	}
+     
+	@AfterSuite
+	public void tearDown() {
+		driver.quit();
+	}
+	
+}
